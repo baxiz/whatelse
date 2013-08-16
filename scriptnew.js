ee5db873b2612c6f552d066dc00fc23bb0e22ed6
sarwesh <sarwesh.krshnn@gmail.com>
2013-08-16 12:59:01 +0530

    first commit

--- scriptnew.js
+++ scriptnew.js
@@ -8,7 +8,7 @@ window.requestAnimFrame = (function() {
 var can, ctx, back, witch, bat; //for canvas and drawimage
 
 var batht=function() {					//for height of bats appearing			
-		this.ht=Math.random()*200+50;
+		this.ht=100;
 	}
 	
 var broom=function() {					//has coords of broom
@@ -16,17 +16,12 @@ var broom=function() {					//has coords of broom
 		this.shipY=140;
 }
 
-var bat=function(){
-		this.shipX=0;
-		this.xIncr=5;
-}
-
 var background= function() {			//var for scrolling background
 		this.xBack=0;
 		this.xIncr=2;
 }
 
-var pumpkin= function(){
+var pumpkinhit= function(){
 		this.px;
 		this.py;
 		this.v;
@@ -34,33 +29,76 @@ var pumpkin= function(){
 }
 
 var points = function(){
-		this.arr=Math.random()*350+50;
+		this.arr;
 }
 
 var collected= function(){
 		this.score=0;
 }
 
+var height = new batht();
 var broomht = new broom();
 var changebg = new background();
-var changebat = new bat();
 var sc=new collected();
+var pumpkin = new pumpkinhit(); 
 	
 var fruit1 = new points();
 var fruit2 = new points();
+fruit1.arr=50;
+fruit2.arr=200;
 
-var height = new batht();
-var height1 = new batht();
-var height2= new batht();
+			var d = new Date();
+            var dnw = new Date();
+            var startTime,time;
+            var deg = 45;
+            var te = 0.1;
+            var i = 0; 
+         	
+            
+            // movement and inertial elements 
+            var right = false;
+            var left = false;
+            var down = false;
+            var up = false;
+            var accx = 2,accy = 2,direction,inertial_speed;
+           
+            // broom 
+            var broomReady = false;
+            var broom = new Image();
+            
+            //pumpkin 
+            var pumpkinReady = false;
+            var pumpkinimage = new Image();
+			
+			// to move broom 
+            function onKeyDown(e) {
+            	  inertial_speed = 0.01;
+            	  if (e.keyCode == 39){right = true ; direction = 'right'; accx = 2; inertial_speed = inertial_speed + 0.001;}
+            	  if (e.keyCode == 37){left = true; direction = 'left'; accx = 2;    inertial_speed = inertial_speed + 0.001;}
+            	  if (e.keyCode == 40){up = true; direction = 'up'; accy = 2;        inertial_speed = inertial_speed + 0.001;}
+              	  if (e.keyCode == 38){down = true; direction = 'down'; accy = 2;    inertial_speed = inertial_speed + 0.001;}
+            	}
+            
+            function onKeyUp(e) {
+            	  if (e.keyCode == 39) right = false;  
+            	  if (e.keyCode == 37) left = false;
+            	  if (e.keyCode == 40) up = false;
+            	  if (e.keyCode == 38) down = false;
+            	}
+            
+            function acceleration()
+            {
+            	
+            	
+            
+            }
 
+			
 var target=[1,1,1,1,1],target1 = [1,1,1,1],target2 = [1,1,1,1] ,targetX = [], targetY = [], targetSpeed = 1.5,
             phase = 0.1, targets = 4, bullets = [1,1,1,1,1], bulletX, bulletY;
  
 function init() {
-			pumpkin=document.getElementById("pumpkin");
-            witch=document.getElementById("witch");
-			back = document.getElementById("back");
-            bat = document.getElementById("bat");
+			var back=document.getElementById("back")
             can = document.getElementById("can");
             ctx = can.getContext("2d");
             ctx.font = "14pt Helvetica";
@@ -71,6 +109,23 @@ function init() {
 			can.addEventListener("mousemove",move, false);
             can.addEventListener("touchmove",tMove, false);
             can.addEventListener("mouseup",newBullet, false);
+			addEventListener('keydown',onKeyDown,false);
+            addEventListener('keyup',onKeyUp,false);
+            time = d.getTime();
+        }
+		
+function newBullet(e) {
+        	
+        	
+        	//pumpkin_array[0].px = 35;
+        	//pumpkin_array[0].py = broomht.shipY + 10; 
+        	
+        	pumpkin.px = broomht.shipX + 35;
+            pumpkin.py = broomht.shipY + 10;
+            te = 0;
+        
+       //     bullet.push(pumpkin.px); 
+         
         }
  
 function newTargets() {
@@ -79,19 +134,6 @@ function newTargets() {
                 targetX[i] = can.width + 10 + i * 50;
                 targetY[i] = i * Math.PI / 2;
             }
-        }
- 
-        // There is a maximum of 1 bullet
-        // If user shoots again, old bullet is gone
-        // If bulletX = 0, there is no bullet
- 
-        // User clicked mouse or lifted finger
-        function newBullet(e) {
-        	bulletX = 35;
-            bulletY = broomht.shipY + 10;
-        
-            bullet.push(bulletX);
-        
         }
  
 function animate() {
@@ -104,19 +146,14 @@ function animate() {
             //checkcollision();
 			//drawTarget();
             drawScore();
+			
+			time = (new Date()).getTime() - startTime;
 					
 			// if background scrolled off screen, reset
             if (changebg.xBack <= -1 * 1200)
             	{
 					changebg.xBack += 1200;
-					
-					if(changebat.xBack<-1200)
-					changebat.xBack += 1200;
-					
 					height.ht=Math.random()*200+50;
-					height1.ht=Math.random()*200+50;
-					height2.ht=Math.random()*250+50;
-					
 					fruit1.arr=Math.random()*350+50;
 					fruit2.arr=Math.random()*350+50;
 					target1=[1,1,1,1];
@@ -142,18 +179,33 @@ function animate() {
  
 function model() {
             // if there is a bullet, advance it
-            if (bulletX)
-                bulletX = bulletX + 2;
-            // if the bullet goes off the right edge, zero it
-            if (bulletX > can.width)
-                 bulletX = 0;
-             // move targets
+            // if there is a pumpkin, advance it 
+            if (pumpkin.px)
+            { 
+              var speed = 3500;
+              var gravity = 250;
+              var t  = time/1000;
+              
+                pumpkin.px = pumpkin.px + speed*t;
+                pumpkin.py = pumpkin.py + (0.5*gravity*te*te);
+                te = te + 0.004;
+      
+             }
+            // if the pumpkin goes off the right edge, zero it 
+            if (pumpkin.px > can.width)
+                 pumpkin.px = 0;
+            if (pumpkin.py > can.height)
+                 te = 0;
+        
+            
             for (i = 0; i < targets; i++) {
                     targetX[i] -= targetSpeed;
                     targetY[i] += phase;
             }
-            // if last target off left edge of screen,
-            // generate new set
+          
+            
+            // if last target off left edge of screen, 
+            // generate new set 
             if (targetX[targets - 1] < 0)
                 newTargets();
         }
@@ -161,22 +213,47 @@ function model() {
 function drawBack() {
                 // pan background                
                 changebg.xBack -= changebg.xIncr;
-				changebat.xBack -= changebat.xIncr;
-				
                 ctx.drawImage(back, changebg.xBack, 0 ,1200, 500);			//800--> image width
                 // draw new copy at right edge of old copy
                 ctx.drawImage(back, changebg.xBack +1200 , 0 ,1200 , 500);
 		}
 		
 function drawWitch() {
-			ctx.drawImage(witch,broomht.shipX,broomht.shipY,100,100);
-		}
+     // load sprite  
+            var t  = time/1000;
+            var speed = 3500;
+            var slow = 1.03-inertial_speed;
+         
+			broom.onload = function () {
+            	broomReady = true;
+            };
+            	broom.src = "witch.png";
+         
+            	  
+            	 // for direction movement 
+            	   if (right) broomht.shipX += speed*t;
+                else 
+                   if (left) broomht.shipX -= speed*t;
+                else
+            	   if (up) broomht.shipY += speed*t;
+                else 
+                   if (down) broomht.shipY -= speed*t;
+            	   
+            	   // for inertia 
+            	      if (!right && direction == 'right') {broomht.shipX += accx; slow = 1.05-inertial_speed; accx = accx/slow;}
+                   else 
+                      if (!left && direction == 'left') {broomht.shipX -= accx; slow = 1.05-inertial_speed; accx = accx/slow;}
+                   else
+               	      if (!up && direction == 'up') {broomht.shipY += accy; slow = 1.05-inertial_speed; accy = accy/slow;}
+                   else 
+                      if (!down && direction == 'down') {broomht.shipY -= accy; slow = 1.05-inertial_speed; accy = accy/slow;}
+
+            ctx.drawImage(broom, broomht.shipX, broomht.shipY, 75, 75);
+			}
 	
 function drawBats(){       	
-			ctx.drawImage(bat, changebat.xBack +700,height.ht ,75 , 75);
-			ctx.drawImage(bat, changebat.xBack +850,height1.ht ,75 , 75);
-			ctx.drawImage(bat, changebat.xBack +1000,height2.ht ,75 , 75);
-			}
+			ctx.drawImage(bat, changebg.xBack +700,height.ht ,100 , 100);
+		}
 		
 function drawPumpkins() {
 				
@@ -202,8 +279,7 @@ function drawPumpkins() {
 					if(target2[i])
 						{
 						ctx.drawImage(pumpkin, changebg.xBack +800 + k2,fruit2.arr+tY ,25 ,25);
-						alert(changebg.xBack +800 + k2);
-						
+												
 						if((changebg.xBack+800+k2)<100 && (changebg.xBack+800+k2)>0)		//subtract x axis of witch to compare
 							{
 								if((broomht.shipY-fruit2.arr)<50 && (broomht.shipY-fruit2.arr)>-100)
@@ -240,8 +316,18 @@ function drawTarget() {
         }
  
 function drawBullet() {					//pumpkin here
-            if (bulletX)
-                ctx.fillRect(bulletX, bulletY, 2, 1);
+            if (pumpkin.px)
+              { 	//ctx.fillRect(pumpkin.px, pumpkin.py, 2, 1);
+            
+                pumpkinimage.onload = function () {
+            	pumpkinReady = true;
+                };
+                
+            	pumpkinimage.src = "pumpkin_hit.png";
+            	
+            ctx.drawImage(pumpkinimage, pumpkin.px, pumpkin.py, 25, 25);
+          //  ctx.rotate(pumpkin, deg * Math.PI/180);
+          }
         }
  
 /*function checkcollision(){
